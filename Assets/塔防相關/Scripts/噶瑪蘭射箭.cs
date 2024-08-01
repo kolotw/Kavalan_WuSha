40c2848aa2a39ebc5576bf5a8101a72f67acf2fc
kolotw <kolo.tw@gmail.com>
2024-08-01 17:13:45 +0800

    修改噶瑪蘭男弓箭手瞄準與射擊，程式。

--- Assets/塔防相關/Scripts/噶瑪蘭射箭.cs
+++ Assets/塔防相關/Scripts/噶瑪蘭射箭.cs
@@ -5,15 +5,18 @@ using UnityEngine.Animations.Rigging;
 
 public class 噶瑪蘭射箭 : MonoBehaviour
 {
+    //瞄準相關
     Animator anim;
-    public GameObject 左手;
-    public GameObject 右手;
     public Transform 目標;
     public Rig rig;
     
+    //產生物件
     public GameObject 箭;
     public Transform 箭的位置;
-
+    GameObject arrow;
+    
+    //控制箭的位置
+    bool 顯示箭 = false;    
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +28,24 @@ public class 噶瑪蘭射箭 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            anim.SetTrigger("FIRE");
+            顯示箭 = true;
+        }
+        if (顯示箭)
+        {
+            rig.weight += 0.2f;
+            if (arrow != null)
+            {
+                arrow.transform.position = 箭的位置.position;
+                arrow.transform.eulerAngles = 箭的位置.eulerAngles;
+            }
+        }
+        else
+        {
+            rig.weight -= 0.1f;
+        }
     }
     
     public void 左手瞄準()
@@ -34,14 +54,17 @@ public class 噶瑪蘭射箭 : MonoBehaviour
     }
     public void 拿出箭()
     {
-
+        arrow = Instantiate(箭,箭的位置.position,Quaternion.identity);
+        Destroy(arrow, 2f);
     }
     public void 拉弓()
     {
 
     }
-    public void 射箭()
+    public void 射出箭()
     {
-
+        print("射出箭");
+        顯示箭 = false;
+        arrow.GetComponent<Rigidbody>().velocity = arrow.transform.up * 10f;
     }
 }
