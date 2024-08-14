71e34bb3ac5d1284e23c6c4622508fd85414790a
YangHoKai <kevin0l29245167@gmail.com>
2024-08-14 20:19:11 +0800

    直書0814

---
+++ Assets/EBook/Scripts/Button/MenuSceneController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void Chapter1()
+    {
+        SceneManager.LoadScene("Chapter1");
+    }
+    public void Chapter2()
+    {
+        SceneManager.LoadScene("Chapter2");
+    }
+    public void Chapter3()
+    {
+        SceneManager.LoadScene("Chapter3");
+    }
+    public void Chapter4()
+    {
+        SceneManager.LoadScene("Chapter4");
+    }
+    public void Chapter5()
+    {
+        SceneManager.LoadScene("Chapter5");
+    }
+    public void Chapter6()
+    {
+        SceneManager.LoadScene("Chapter6");
+    }
+    public void Chapter7()
+    {
+        SceneManager.LoadScene("Chapter7");
+    }
+    public void Chapter8()
+    {
+        SceneManager.LoadScene("Chapter8");
+    }
+    public void Chapter9()
+    {
+        SceneManager.LoadScene("Chapter9");
+    }
+}
