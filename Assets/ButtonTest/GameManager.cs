5ae42767c8f183054f22eb912d56e2522f6d901a
kolotw <kolo.tw@gmail.com>
2024-08-22 13:13:17 +0800

    0822調整至會場展演，增加DEMO場景

--- Assets/ButtonTest/GameManager.cs
+++ Assets/ButtonTest/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -19,24 +20,30 @@ public class GameManager : MonoBehaviour
     {
         print("hello");
     }
-    public void button2()
+    public void Ch1_WuSha()
     {
-        
+        SceneManager.LoadScene("Chapter1");
     }
-    public void button3()
+    public void Ch2_澤蘭宮()
     {
-
+        SceneManager.LoadScene("Chapter2");
     }
-    public void button4()
+    public void Ch3_龍潭湖社區()
     {
-
+        SceneManager.LoadScene("Chapter3");
     }
-    public void button5()
+    public void Ch4_哲思書棧()
     {
-
+        SceneManager.LoadScene("Chapter4");
     }
-    public void button6() { }
-    public void button7() { }
+    public void lv1() 
+    {
+        SceneManager.LoadScene("試做第四關(整理到可以玩)");
+    }
+    public void 宜蘭地形() 
+    {
+        SceneManager.LoadScene("0820_3D地形展演");
+    }
     public void button8() { }
     public void button9() { }
 }
